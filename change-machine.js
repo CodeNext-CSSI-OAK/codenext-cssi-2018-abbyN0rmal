d83a5342a0348cf7714749698d35507b4d4a54ae
Student <student@Admins-MacBook-Air.local>
2018-07-10 16:37:37 -0700

    update

--- change-machine.js
+++ change-machine.js
@@ -1,1 +1,27 @@
-// Author: FirstName LastName
+// Author: Abby Guerrero
+
+console.log("________________________________________________________________________________________");
+
+console.log("BLEEP! bLOOP! Welcome to your personal cHaNgE maChInE!");
+
+let readline = require ("readline-sync");
+let amount = Number(readline.question("Please enter your amount with positive numbers: "));
+
+let Quarters = Math.floor (amount / 25) ;
+console.log("Your amount in Quarters = " + Quarters);
+
+let remainderAfterQuarters = Math.floor(amount % 25);
+let Dimes = Math.floor( remainderAfterQuarters / 10);
+console.log("Your amount in Dimes = " + Dimes);
+
+let remainderAfterDimes = Math.floor( remainderAfterQuarters % 10);
+let Nickels = Math.floor( remainderAfterDimes / 5);
+console.log("Your amount in Nickels = " + Nickels);
+
+let remainderAfterNickels = Math.floor( remainderAfterDimes % 5);
+let Pennies = Math.floor( remainderAfterNickels / 1);
+console.log("Your amount in Pennies = " + Pennies);
+
+console.log("This is your total amount converted into COINS!!");
+
+console.log("________________________________________________________________________________________");
