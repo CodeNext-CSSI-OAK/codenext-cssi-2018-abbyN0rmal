602021856a6f936a084a0bed8b3d47bec51977e6
Student <student@Admins-MacBook-Air.local>
2018-07-10 09:32:59 -0700

    update

--- easter-predictor.js
+++ easter-predictor.js
@@ -3,43 +3,43 @@ let y = 2003;
 console.log ("y = 2003");
 
 let a = y % 19;
-console.log(a);
+console.log("a = " + y % 19);
 
-let b = Math.floor(y / 100);
-console.log(b);
+let b = Math.floor (y / 100);
+console.log("b = " + y / 100);
 
 let c = y % 100;
-console.log(c);
+console.log("c = " + c);
 
 let d = b / 4;
-console.log(d);
+console.log("d = " + d);
 
 let e =  b % 4;
-console.log(e);
+console.log("e = " + e);
 
-let f = Math.floor (b + 8) / 25;
-console.log(f);
+let f = (b + 8) / 25;
+console.log("f = " + f);
 
-let g = Math.floor (b - f + 1) / 3;
-console.log(g);
+let g = (b - f + 1) / 3;
+console.log("g = " + g);
 
- let h = Math.floor (19 * a + b - d - g + 15) / 30;
- console.log(h);
+ let h = (19 * a + b - d - g + 15) / 30;
+ console.log("h = " + h);
 
  let i = c / 4;
- console.log(i);
+ console.log("i = " + i);
 
  let k = c % 4;
- console.log(k);
+ console.log("k = " + k);
 
  let r = (32 + 2 * e + 2 * i - h - k) % 7;
- console.log(r);
+ console.log("r = " + r);
 
  let m = (a + 11 * h + 22 * r) / 451;
-console.log(m);
+console.log("m = " + m);
 
 let n = (h + r - 7 * m + 114) / 31;
-console.log(n);
+console.log("n = " + n);
 
 let p = (h + r - 7 * m + 114) % 31;
-console.log(p);
+console.log("p = " + p);
