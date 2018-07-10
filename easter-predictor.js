927dbc43b3b7e0285171eb584b0194981201d769
Student <student@Admins-MacBook-Air.local>
2018-07-10 10:41:41 -0700

    update

--- easter-predictor.js
+++ easter-predictor.js
@@ -1,12 +1,12 @@
 // Author: Abigail Guerrero
 let y = 2003;
-console.log ("y = 2003");
+console.log ("y = " + y);
 
 let a = y % 19;
-console.log("a = " + y % 19);
+console.log("a = " + a);
 
-let b = Math.floor (y / 100);
-console.log("b = " + y / 100);
+let b = Math.floor(y / 100) ;
+console.log("b = " + b);
 
 let c = y % 100;
 console.log("c = " + c);
@@ -17,16 +17,16 @@ console.log("d = " + d);
 let e =  b % 4;
 console.log("e = " + e);
 
-let f = (b + 8) / 25;
+let f = Math.floor((b + 8) / 25);
 console.log("f = " + f);
 
-let g = (b - f + 1) / 3;
+let g = Math.floor((b - f + 1) / 3);
 console.log("g = " + g);
 
- let h = (19 * a + b - d - g + 15) / 30;
+ let h = (19 * a + b - d - g + 15) % 30;
  console.log("h = " + h);
 
- let i = c / 4;
+ let i = Math.floor(c / 4);
  console.log("i = " + i);
 
  let k = c % 4;
@@ -35,11 +35,13 @@ console.log("g = " + g);
  let r = (32 + 2 * e + 2 * i - h - k) % 7;
  console.log("r = " + r);
 
- let m = (a + 11 * h + 22 * r) / 451;
+ let m = Math.floor((a + 11 * h + 22 * r) / 451);
 console.log("m = " + m);
 
-let n = (h + r - 7 * m + 114) / 31;
+let n = Math.floor((h + r - 7 * m + 114) / 31);
 console.log("n = " + n);
 
-let p = (h + r - 7 * m + 114) % 31;
-console.log("p = " + p);
+let p = ((h + r - 7 * m + 114) % 31);
+console.log("p = " + (p + (1)));
+
+console.log(" In 2003, Easter falls on 4/20");
